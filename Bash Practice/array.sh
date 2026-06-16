7eb640210af5e3780c60655beb21979d7a4f9f5c
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2026-06-16 22:43:15 +0600

    DevOps (Bash Script): array

---
+++ Bash Practice/array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+declare -a myArray=("Emrul" "Hasan" "Emon")
+
+echo "First element: ${myArray[0]}"
+# Output: First element: Emrul
+
+echo "Second element: ${myArray[1]}"
+# Output: Second element: Hasan
+
+echo "Third element: ${myArray[2]}"
+# Output: Third element: Emon
