c67f848b4e258d8735103a3e83338f0bfa0a933e
Emrul Hasan Emon <emrulhasanemon12@gmail.com>
2026-06-16 22:34:35 +0600

    DevOps (Bash Script): system variables

---
+++ Bash Practice/system_variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Home: $HOME"
+# Output: Home: /home/emrul
+
+echo "User: $USER"
+# Output: User: emrul
+
+echo "Shell: $SHELL"
+# Output: Shell: /bin/bash
+
+echo "Current Directory: $PWD"
+# Output: Current Directory: /home/emrul
+
+echo "Path: $PATH"
+# Output: Path: /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin/
